1f93c0a10c2b63f7a9bd4c0acbc5a37f5ba63455
Jon Arbaugh <jonarbaugh@gmail.com>
2014-07-22 00:05:22 -0400

    Make a basic line chart of tweets per day

---
+++ static/js/index.js
@@ -0,0 +1,96 @@
+
+/*
+ * data is an array of tweets
+ *
+ * A breakdown of a tweet:
+ * http://dev.twitter.com/docs/platform-objects/tweets
+ */
+d3.json('mings_tweets.json', function (err, data) {
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    var x = d3.time.scale()
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var line = d3.svg.line()
+        .x(function (d) {
+            return x(d.date);
+        })
+        .y(function (d) {
+            return y(d.count);
+        });
+
+    var svg = d3.select('svg.chart')
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.json('mings_tweets.json', function (err, data) {
+        if (err) {
+            console.error('Could not load ming\'s tweets!');
+            return;
+        }
+
+        // Group the tweets by day
+        var dateTable = {};
+        data.forEach(function (d) {
+            if (!d.created_at) {
+                return;
+            }
+            var date = new Date(d.created_at);
+            date.setHours(0, 0, 0, 0);
+            var key = date.getTime();
+            if (dateTable[key]) {
+                dateTable[key]++;
+            } else {
+                dateTable[key] = 1;
+            }
+        });
+
+        var dates = [];
+        var keys = Object.keys(dateTable);
+        keys.forEach(function (key) {
+            dates.push({
+                date: key,
+                count: dateTable[key]
+            });
+        });
+
+        x.domain(d3.extent(dates, function (d) { return d.date; }));
+
+        y.domain(d3.extent(dates, function (d) { return d.count; }));
+
+        svg.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0,' + height + ')')
+            .call(xAxis);
+
+        svg.append('g')
+            .attr('class', 'y axis')
+            .call(yAxis)
+            .append('text')
+            .attr('transform', 'rotate(-90)')
+            .attr('y', 6)
+            .attr('dy', '.71em')
+            .style('text-anchor', 'end')
+            .text('# Tweets');
+
+        svg.append('path')
+            .datum(dates)
+            .attr('class', 'line')
+            .attr('d', line);
+    });
+});
